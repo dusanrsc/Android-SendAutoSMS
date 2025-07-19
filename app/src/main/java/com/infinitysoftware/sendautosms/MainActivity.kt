8fec6a89003522c78e887df2b0b153bf11a0b80f
dusanrsc <dusanrosic25.06.1997@gmail.com>
2025-07-19 17:03:50 +0200

    Added Not Fully Funtional GUI

--- app/src/main/java/com/infinitysoftware/sendautosms/MainActivity.kt
+++ app/src/main/java/com/infinitysoftware/sendautosms/MainActivity.kt
@@ -7,8 +7,23 @@ import android.telephony.SmsManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
+import com.infinitysoftware.sendautosms.ui.theme.defaultBackgroundColor
 
 class MainActivity : ComponentActivity() {
 
@@ -34,7 +49,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MaterialTheme {
                 Surface {
-                    Text("The Message Has Been Sent Automatically.")
+                    GUI()
                 }
             }
         }
@@ -46,6 +61,41 @@ class MainActivity : ComponentActivity() {
             smsManager.sendTextMessage(phoneNumber, null, message, null, null)
         } catch (e: Exception) {
             e.printStackTrace()
+        }
+    }
+}
+
+@Composable
+fun GUI() {
+
+    // Composable Remember Variable Section.
+    var phoneNumber by remember { mutableStateOf("") }
+    var textMessage by remember { mutableStateOf("") }
+
+    // Actual GUI/UI Section.
+    Column(modifier = Modifier.fillMaxSize().background(defaultBackgroundColor), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+
+        // Enter Phone Number Text Field.
+        TextField(
+            value = phoneNumber,
+            onValueChange = { phoneNumber = it },
+            label = { Text(text = "Enter Phone Number") }
+        )
+
+        Spacer(modifier = Modifier.size(10.dp))
+
+        // Enter Text Content Of The Message.
+        TextField(
+            value = textMessage,
+            onValueChange = {textMessage = it},
+            label = { Text(text = "Enter Message") }
+        )
+
+        Spacer(modifier = Modifier.size(20.dp))
+
+        // Proceed Button - If Phone Number And Text Message Are Valid Send Message!
+        Button(onClick = { /* TODO */ }){
+            Text(text = "Proceed!")
         }
     }
 }
